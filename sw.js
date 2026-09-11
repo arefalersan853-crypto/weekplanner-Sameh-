afaaa203739045b5131afa2ec74ea48232a2de20
arefalersan853-crypto <arefalersan853@gmail.com>
2026-09-11 15:02:05 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,7 @@
-const CACHE="weekplanner-v1";self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(["./","./index.html","./manifest.json"]))));self.addEventListener("fetch",e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
+const CACHE="weekplanner-v2";
+self.addEventListener("install",e=>{
+  self.skipWaiting();
+  e.waitUntil(caches.open(CACHE).then(c=>c.addAll(["./","./index.html","./manifest.json"])));
+});
+self.addEventListener("activate",e=>e.waitUntil(self.clients.claim()));
+self.addEventListener("fetch",e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
